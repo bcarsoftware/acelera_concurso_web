b33d625c1f95898dc270960f60e73e166a668cd1
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-10-27 11:48:50 -0300

    refact: close function dialog confirmation

--- app/dialog/dialog-confirm.tsx
+++ app/dialog/dialog-confirm.tsx
@@ -6,36 +6,29 @@ interface DialogParams {
     message: string;
     isOpen: boolean;
     yesFunction: () => void;
+    closeFunction: (arg: boolean) => void;
 }
 
 export const DialogConfirm = ({
-   title, message, isOpen, yesFunction,
+   title, message, yesFunction, closeFunction
 }: DialogParams) => {
-    const [open, setOpen] = useState<boolean>(isOpen);
-
-    const close = () => setOpen(false);
-
     return (
         <>
-            {open && (
-                <>
-                    <Style />
-                    <div className={"popup-overlay"}>
-                        <div className={"dialog-container"}>
-                            <div id={"TitleDiv"}>
-                                <div id={"TextDiv"}>
-                                    <h2>{title}</h2>
-                                </div>
-                            </div>
-                            <p>{message}</p>
-                            <div id={"ButtonDiv"}>
-                                <button className={"button-yes"} onClick={yesFunction}>SIM</button>
-                                <button className={"button-not"} onClick={close}>NÃO</button>
-                            </div>
+            <Style />
+            <div className={"popup-overlay"}>
+                <div className={"dialog-container"}>
+                    <div id={"TitleDiv"}>
+                        <div id={"TextDiv"}>
+                            <h2>{title}</h2>
                         </div>
                     </div>
-                </>
-            )}
+                    <p>{message}</p>
+                    <div id={"ButtonDiv"}>
+                        <button className={"button-yes"} onClick={yesFunction}>SIM</button>
+                        <button className={"button-not"} onClick={() => closeFunction(false)}>NÃO</button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
